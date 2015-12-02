d8aefe09ac2b90642e8dac32d7c2e1107fba3086
Rahul <rahuln@clavax.com>
2015-12-02 06:29:39 +0530

    link-list

---
+++ link-list.c
@@ -0,0 +1,23 @@
+/*
+	linked list: https://en.wikipedia.org/wiki/Linked_list
+
+	use sentinel node: https://en.wikipedia.org/wiki/Sentinel_node
+	
+	Description-------
+	store information with boundary mark record
+*/
+
+
+// file ops
+void readListFromFile();
+void writeListToFile();
+
+// linked list ops
+void saveToList();
+void searchFromList();
+void * createNode();
+
+struct 
+
+
+
