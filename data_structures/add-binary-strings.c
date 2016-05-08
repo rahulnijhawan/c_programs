0da2a6c07835a2edd701b669d727d94caf686036
Rahul <rahuln@clavax.com>
2016-05-08 19:00:22 +0530

    add binary strings

--- data_structures/add-binary-strings.c
+++ data_structures/add-binary-strings.c
@@ -1,17 +1,54 @@
 #include <stdio.h>
+#include <string.h> // strlen, memmove, memset
 
+#define BINARY_STRING_LEN 20
 
 enum bool{false, true};
 
 int is_binary_digit(char c) {
-	int zero_char_int_value = 48;
+	int zero_char_int_value = '0';
 	int digit = (int) c - zero_char_int_value;
-
+	
 	if (digit == 0 || digit == 1) {
 		return true;
 	}
 	return false;
 }
+
+int is_binary_string(const char *bin_str) {
+	
+	// find search methond other than linear search.?????
+	// string end with null charachter
+	while (*bin_str != '\0') {
+		if (is_binary_digit(*bin_str) == false) {
+			return false;
+		} 
+		bin_str++;
+	}
+	return true;
+}
+
+void * prepend_zeros(char *bin_str) {
+
+	/*
+		moving chars to end and then prepend zeros is expensive? or find other options???
+		-- memmove
+	*/
+	char temp[BINARY_STRING_LEN];
+	int len = strlen(bin_str);
+	int diff = BINARY_STRING_LEN - len - 1;
+	printf("len %d, diff %d\n", len, diff);
+	if (diff > 0) {
+		memmove((bin_str + diff), bin_str, len);
+		memset(bin_str, (int) '0', diff );
+	}
+
+	return bin_str;
+}
+
+
+
+
 char * add_sol1(char *bin_str1, char* bin_str2) {
 	/*
 		access one character from each from the end(left) and it using modula 2
@@ -25,15 +62,43 @@ char * add_sol1(char *bin_str1, char* bin_str2) {
 			6-- assing modula result to carry variable and divided(devide result(have to check)) 
 				result to variable created at step 3.
 			7-- reverse the string or use some other way to not reverse it(for example using one of them.) 
+	
+		validation
+			-- must be of equal length or make it equal by prepening zeros.
+			-- test for binary string.
 	*/
+	int i = 0;
+	int carry = 0;
+	int str1_len, str2_len;
+ 	// check for binary string????
+	prepend_zeros(bin_str1);
+	prepend_zeros(bin_str2);
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	for(i = BINARY_STRING_LEN-2; i >=0; i--) {
+		//printf("%c, %c\n", *(bin_str1 + i), *(bin_str2 + i));
+		num1 = *(bin_str1 + i) - '0';
+		num2 = *(bin_str2 + i) - '0';
+		sum = num1 + num2 + carry;
+		printf("num1 %d, num2 %d, carry %d, sum((num1 + num2 + carry) / 2) %d\n",num1, num2, carry, sum);  
+		
+		carry = sum / 2;
+		bin_str1[i] = (sum % 2) + '0';
 
-
+	}	
+	return bin_str1;
 }
 
 int main(int argc, char const *argv[])
 {
-	printf("%d\n", is_binary_digit('0'));
-	printf("%d\n", is_binary_digit('2'));
-	printf("%d\n", is_binary_digit('1'));
+
+	printf("is_binary_digit %d\n", is_binary_digit('0'));
+	printf("is_binary_digit %d\n", is_binary_digit('2'));
+	//printf("is_binary_string %d\n", is_binary_string(argv[1]));
+	char bin_str1[BINARY_STRING_LEN] = "1111000000000000000\0";
+	char bin_str2[BINARY_STRING_LEN] = "11111111\0";
+	//printf("%s\n", (char *) prepend_zeros(bin_str1));
+	printf("%s\n", add_sol1(bin_str1, bin_str2));
 	return 0;
 }
---
+++ fifo-file-reader.c
@@ -0,0 +1,7 @@
+/*
+	reader for FIFO_FILE
+
+*/
+
+
+#define FIFO_FILE "fifo_file"
